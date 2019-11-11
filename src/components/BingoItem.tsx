28985142d4a47e95cfa264d28b8271b6e28e23d1
mango906 <minkb01@gmail.com>
2019-11-11 11:55:19 +0900

    [FIX] 약간의 코드 수정
    interface 안의 속성들이 ; 가 아니라 ,로 되있었다.

--- src/components/BingoItem.tsx
+++ src/components/BingoItem.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 interface Props {
-   rowNumber: number,
-   columnNumber: number
+  rowNumber: number;
+  columnNumber: number;
 }
 
 const BingoItem = ({ rowNumber, columnNumber }: Props) => {
-   const Item = styled.div`
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 80px;
-      height: 80px;
-      background-color: #fff;
-      font-size: 2rem;
-   `
+  const Item = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 80px;
+    height: 80px;
+    background-color: #fff;
+    font-size: 2rem;
+  `;
 
-   return (
-      <Item>😍</Item>
-   );
+  return <Item>😍</Item>;
 };
 
 export default BingoItem;
