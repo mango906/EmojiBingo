78eedcc70c81eea1d52608b458f3a722d94049a7
민경빈 <minkb01@gmail.com>
2019-11-11 23:50:08 +0900

    [FIX] styled-componets 선언부분 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import BingoBoard from "./containers/BingoBoard";
 
-const App: React.FC = () => {
-  const Wrapper = styled("div")`
-    background-color: #282c34;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-content: center;
-  `
+const Wrapper = styled("div")`
+   background-color: #282c34;
+   width: 100vw;
+   height: 100vh;
+   display: flex;
+   justify-content: center;
+   align-content: center;
+`;
 
-  return (
-    <Wrapper>
-      <BingoBoard />
-    </Wrapper>
-  );
-}
+const App: React.FC = () => {
+   return (
+      <Wrapper>
+         <BingoBoard />
+      </Wrapper>
+   );
+};
 
 export default App;
--- src/containers/BingoBoard.tsx
+++ src/containers/BingoBoard.tsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import BingoItem from '../components/BingoItem';
+import React, { useState } from "react";
+import styled from "styled-components";
+import BingoItem from "../components/BingoItem";
 
 interface CountProps {
    count: number;
 }
 
 interface Square {
-   contents: string,
-   checked: boolean
+   contents: string;
+   checked: boolean;
 }
 
+const Wrapper = styled("div")`
+   display: flex;
+   flex-basis: content;
+   position: relative;
+   align-items: center;
+`;
+
+const Board = styled("div")<CountProps>`
+   display: grid;
+   grid-template-columns: repeat(${props => props.count}, 5rem);
+   grid-template-rows: repeat(${props => props.count}, 5rem);
+   grid-gap: 5px;
+`;
+
 const BingoBoard = () => {
-   const Wrapper = styled("div")`
-      display: flex;
-      flex-basis: content;
-      position: relative;
-      align-items: center;
-   `
-
-   const Board = styled("div") <CountProps>`
-      display: grid;
-      grid-template-columns: repeat(${props => props.count}, 5rem);
-      grid-template-rows: repeat(${props => props.count}, 5rem);
-      grid-gap: 5px;
-   `
-
    const [count, setCount] = useState<number>(5);
 
    let matrix = [];
@@ -39,7 +39,12 @@ const BingoBoard = () => {
          <Board count={count}>
             {matrix.map((row: Square[], rowNumber: number) =>
                row.map((el: Square, columnNumber: number) => {
-                  return <BingoItem rowNumber={rowNumber} columnNumber={columnNumber} />
+                  return (
+                     <BingoItem
+                        rowNumber={rowNumber}
+                        columnNumber={columnNumber}
+                     />
+                  );
                })
             )}
          </Board>
