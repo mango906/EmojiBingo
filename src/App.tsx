47f34b4f6e806b4fbdad021c73e21da86c809545
mango906 <minkb01@gmail.com>
2019-11-12 17:58:40 +0900

    [FIX] 빙고판 크기를 수정할수 있게

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import BingoBoard from "./containers/BingoBoard";
 
-const App: React.FC = () => {
-   const Wrapper = styled("div")`
-      background-color: #282c34;
-      width: 100vw;
-      height: 100vh;
-      display: flex;
-      align-itmes: center;
-      justify-content: center;
-   `;
+const Wrapper = styled("div")`
+   background-color: #282c34;
+   width: 100vw;
+   height: 100vh;
+   display: flex;
+   justify-content: center;
+   align-content: center;
+`;
 
+const App: React.FC = () => {
    return (
       <Wrapper>
          <BingoBoard />
--- src/components/BingoItem.tsx
+++ src/components/BingoItem.tsx
@@ -2,22 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 interface Props {
-  rowNumber: number;
-  columnNumber: number;
+   rowNumber: number;
+   columnNumber: number;
 }
 
-const BingoItem = ({ rowNumber, columnNumber }: Props) => {
-  const Item = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 80px;
-    height: 80px;
-    background-color: #fff;
-    font-size: 2rem;
-  `;
+const Item = styled.div`
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   width: 80px;
+   height: 80px;
+   background-color: #fff;
+   font-size: 2rem;
+`;
 
-  return <Item>😍</Item>;
+const BingoItem = ({ rowNumber, columnNumber }: Props) => {
+   return <Item>😍</Item>;
 };
 
 export default BingoItem;
