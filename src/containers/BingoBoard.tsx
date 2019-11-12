53b41198dfb79641f6fa1f59cbc3059f138eabe0
mango906 <minkb01@gmail.com>
2019-11-12 17:50:57 +0900

    [ADD] 보드판 크기 수정할수 있게

--- src/containers/BingoBoard.tsx
+++ src/containers/BingoBoard.tsx
@@ -3,53 +3,75 @@ import styled from "styled-components";
 import BingoItem from "../components/BingoItem";
 
 interface CountProps {
-   count: number;
+  count: number;
 }
 
 interface Square {
-   contents: string;
-   checked: boolean;
+  contents: string;
+  checked: boolean;
 }
 
 const BingoBoard = () => {
-   const Wrapper = styled("div")`
-      display: flex;
-      flex-basis: content;
-      position: relative;
-      align-items: center;
-   `;
+  const Wrapper = styled("div")`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  `;
 
-   const Board = styled("div")<CountProps>`
-      display: grid;
-      grid-template-columns: repeat(${props => props.count}, 5rem);
-      grid-template-rows: repeat(${props => props.count}, 5rem);
-      grid-gap: 5px;
-   `;
+  const Board = styled("div") <CountProps>`
+    display: grid;
+    grid-template-columns: repeat(${props => props.count}, 5rem);
+    grid-template-rows: repeat(${props => props.count}, 5rem);
+    grid-gap: 5px;
+    align-self: center;
+    margin-top: 20px;
+  `;
 
-   const [count, setCount] = useState<number>(5);
+  const Header = styled("h1")`
+    color: #fff;
+    text-align: center;
+  `;
 
-   let matrix = [];
+  const WhiteFont = styled("div")`
+    color: #fff;
+  `;
 
-   for (let i = 0; i < count; i++) {
-      matrix[i] = new Array(count).fill({ contents: "", checked: false });
-   }
+  const [count, setCount] = useState<number>(5);
 
-   return (
-      <Wrapper>
-         <Board count={count}>
-            {matrix.map((row: Square[], rowNumber: number) =>
-               row.map((el: Square, columnNumber: number) => {
-                  return (
-                     <BingoItem
-                        rowNumber={rowNumber}
-                        columnNumber={columnNumber}
-                     />
-                  );
-               })
-            )}
-         </Board>
-      </Wrapper>
-   );
+  let matrix = [];
+  for (let i = 0; i < count; i++) {
+    matrix[i] = new Array(count).fill({ contents: "", checked: false });
+  }
+
+  return (
+    <Wrapper>
+      <Header>😈이모지 빙고👿</Header>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <WhiteFont>개수</WhiteFont>
+        <select
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+            setCount(parseInt(e.target.value))
+          }
+          value={count}
+        >
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+        </select>
+      </div>
+
+      <Board count={count}>
+        {matrix.map((row: Square[], rowNumber: number) =>
+          row.map((el: Square, columnNumber: number) => {
+            return (
+              <BingoItem rowNumber={rowNumber} columnNumber={columnNumber} />
+            );
+          })
+        )}
+      </Board>
+    </Wrapper>
+  );
 };
 
 export default BingoBoard;
