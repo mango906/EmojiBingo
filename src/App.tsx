c5ed14c115b0a779f620f8247543f053ad675f65
mango906 <minkb01@gmail.com>
2019-11-14 15:07:42 +0900

    [FIX] BingoBoard 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import BingoBoard from "./containers/BingoBoard";
 
@@ -7,14 +7,40 @@ const Wrapper = styled("div")`
    width: 100vw;
    height: 100vh;
    display: flex;
-   justify-content: center;
-   align-content: center;
+   flex-direction: column;
+   align-items: center;
+`;
+
+const Header = styled("h1")`
+    color: #fff;
+    text-align: center;
+`;
+
+const WhiteFont = styled("div")`
+    color: #fff;
 `;
 
 const App: React.FC = () => {
+   const [count, setCount] = useState<number>(5);
+
    return (
       <Wrapper>
-         <BingoBoard />
+         <Header>😈이모지 빙고👿</Header>
+         <div style={{ display: "flex", justifyContent: "center" }}>
+            <WhiteFont>개수</WhiteFont>
+            <select
+               onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setCount(parseInt(e.target.value))
+               }
+               value={count}
+            >
+               <option>5</option>
+               <option>6</option>
+               <option>7</option>
+               <option>8</option>
+            </select>
+         </div>
+         <BingoBoard count={count} />
       </Wrapper>
    );
 };
--- src/containers/BingoBoard.tsx
+++ src/containers/BingoBoard.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import BingoItem from "../components/BingoItem";
 import emojis from "../config/config";
 
-interface CountProps {
+interface Props {
   count: number;
 }
 
@@ -12,13 +12,15 @@ interface Square {
   checked: boolean;
 }
 
+
+
 const Wrapper = styled("div")`
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
   `;
 
-const Board = styled("div") <CountProps>`
+const Board = styled("div") <Props>`
     display: grid;
     grid-template-columns: repeat(${props => props.count}, 5rem);
     grid-template-rows: repeat(${props => props.count}, 5rem);
@@ -27,49 +29,24 @@ const Board = styled("div") <CountProps>`
     margin-top: 20px;
   `;
 
-const Header = styled("h1")`
-    color: #fff;
-    text-align: center;
-  `;
+const BingoBoard = ({ count }: Props) => {
 
-const WhiteFont = styled("div")`
-    color: #fff;
-  `;
+  let matrix = [];
 
-const BingoBoard = () => {
-  const [count, setCount] = useState<number>(5);
+  for (let i = 0; i < count; i++) {
+    matrix[i] = new Array(count).fill({ contents: "", checked: false });
+  }
 
   const shuffle = useCallback((array: Array<string>) => {
     for (let i = array.length; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
-      console.log(array);
+      return array;
     }
   }, []);
 
-  let matrix = [];
-  for (let i = 0; i < count; i++) {
-    matrix[i] = new Array(count).fill({ contents: "", checked: false });
-  }
-
   return (
     <Wrapper>
-      <Header>😈이모지 빙고👿</Header>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <WhiteFont>개수</WhiteFont>
-        <select
-          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-            setCount(parseInt(e.target.value))
-          }
-          value={count}
-        >
-          <option>5</option>
-          <option>6</option>
-          <option>7</option>
-          <option>8</option>
-        </select>
-      </div>
-
       <Board count={count}>
         {matrix.map((row: Square[], rowNumber: number) =>
           row.map((el: Square, columnNumber: number) => {
